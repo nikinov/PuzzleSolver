2ef94409c615b494b2aa5bc84f82d02d730f012f
nikinov <54107324+nikinov@users.noreply.github.com>
2022-12-31 12:47:35 +0100

    init project

---
+++ solver.py
@@ -0,0 +1,22 @@
+import cv2
+
+
+class Opener:
+    def __init__(self):
+        pass
+
+    def open_finished_puzzle(self):
+        open
+
+    def open_piece(self):
+        pass
+
+class Detector:
+    def __init__(self):
+        pass
+
+    def set_finished(self, puz, puz_points):
+        pass
+
+    def get_piece_result(self):
+        pass
